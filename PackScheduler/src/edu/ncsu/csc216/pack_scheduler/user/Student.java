2851770884bd8357a7724cafe52bff3a0dd64417
wsmitche <wsmitche@ncsu.edu>
2018-08-26 21:51:08 -0700

    initial zip folder committed to the repo

---
+++ PackScheduler/src/edu/ncsu/csc216/pack_scheduler/user/Student.java
@@ -0,0 +1,24 @@
+package edu.ncsu.csc216.pack_scheduler.user;
+
+public class Student {
+
+	public Student(String firstName, String lastName, String id, String email, String hashPW, int maxCredits) {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public String getFirstName() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public String getLastName() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	public String getId() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
