c15ff2aa7d02d5e9df2791b16131216ef9fdd6b4
wsmitche <wsmitche@ncsu.edu>
2018-09-06 13:57:18 -0700

    created javadocs for studentrecordIO

--- PackScheduler/src/edu/ncsu/csc216/pack_scheduler/io/StudentRecordIO.java
+++ PackScheduler/src/edu/ncsu/csc216/pack_scheduler/io/StudentRecordIO.java
@@ -4,10 +4,25 @@ import java.io.*;
 import java.util.*;
 
 import edu.ncsu.csc216.pack_scheduler.user.Student;
-import edu.ncsu.csc216.wolf_scheduler.course.Course;
+
+/**
+*
+* Reads Student records from text files.  Can also write a set of Student records to a file.
+* 
+* @author William
+*@version 9/6/2018
+*/
 
 public class StudentRecordIO {
-
+	
+	/**
+     * Reads Student records from a file and generates a list of valid Students.  Any duplicate or invalid
+     * Students are ignored.  If the file to read cannot be found or the permissions are incorrect
+     * a File NotFoundException is thrown.
+     * @param fileName file to read Student records from
+     * @return a list of valid Students
+     * @throws FileNotFoundException if the file cannot be found or read
+     */
 	public static ArrayList<Student> readStudentRecords(String fileName) throws FileNotFoundException {
 		Scanner fileReader = new Scanner(new FileInputStream(fileName));
 	    ArrayList<Student> students = new ArrayList<Student>();
@@ -33,6 +48,15 @@ public class StudentRecordIO {
 	    return students;
 	}
 
+	
+	/**
+	 * Reads Student information from a line and creates a Student object from 
+	 * that information and passes it into the Student array list constructor
+	 * 
+	 * @param nextLine from a scanner object that contains student information
+	 * @return student the student that will be added to an array of students
+	 * @throws IllegalArgumentExpression if all fields are not entered correctly.
+	 */
 	private static Student processStudent (String line) {
 		//firstName, String lastName, String id, String email, String password, int maxCredits
 		Scanner lineScanner = new Scanner(line);
@@ -93,6 +117,16 @@ public class StudentRecordIO {
 		
 	}
 	
+	/**
+     * Writes the given list of Courses to a file
+     * The IOException has been caught in the StudentDirectory class
+     * 
+     * @param fileName name of the file to which the directory will be written
+     * @param studentDirectoy Array list of Students to be written to the file
+	 * @throws FileNotFoundException thrown if the file of filename cannot be found
+	 * (this is thrown by StudentDirectory class)
+     * 
+     */
 	public static void writeStudentRecords(String fileName, ArrayList<Student> studentDirectory) throws FileNotFoundException {
 		PrintStream fileWriter = new PrintStream(new File(fileName));
 
