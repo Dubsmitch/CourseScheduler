ca38a16b4866aaf4504842b6c6c4c5df7f7f2396
wsmitche <wsmitche@ncsu.edu>
2018-09-18 16:03:15 -0700

    work on constructor

--- PackScheduler/test/edu/ncsu/csc216/pack_scheduler/user/StudentTest.java
+++ PackScheduler/test/edu/ncsu/csc216/pack_scheduler/user/StudentTest.java
@@ -40,6 +40,7 @@ public class StudentTest {
 
 		// Start testing invalid input for each parameter - one at a time.
 		// The standard structure of an invalid test is the following:
+		// test null firstname
 		s = null;
 		try {
 		    s = new Student(null, "last", "id", "email@ncsu.edu", "hashedpassword");
@@ -50,7 +51,19 @@ public class StudentTest {
 		    //for the test.  We also need to make sure that the reference s is still null!
 		    assertNull(s);
 		}
+		//test empty firstname
+		s = null;
+		try {
+		    s = new Student("", "last", "id", "email@ncsu.edu", "hashedpassword");
+		    //Note that for testing purposes, the password doesn't need to be hashedpassword
+		    fail(); //If we reach this point a Student was constructed when it shouldn't have been!
+		} catch (IllegalArgumentException e) {
+		    //We should get here if the expected IllegalArgumentException is thrown, but that's not enough
+		    //for the test.  We also need to make sure that the reference s is still null!
+		    assertNull(s);
+		}
 		
+		//test null lastname
 		s = null; //Initialize a student reference to null
 		try {
 		    s = new Student("first", null, "id", "email@ncsu.edu", "hashedpassword");
@@ -61,7 +74,20 @@ public class StudentTest {
 		    //for the test.  We also need to make sure that the reference s is still null!
 		    assertNull(s);
 		}
-		
+
+		//test empty lastname
+		s = null; //Initialize a student reference to null
+		try {
+		    s = new Student("first", "", "id", "email@ncsu.edu", "hashedpassword");
+		    //Note that for testing purposes, the password doesn't need to be hashedpassword
+		    fail(); //If we reach this point a Student was constructed when it shouldn't have been!
+		} catch (IllegalArgumentException e) {
+		    //We should get here if the expected IllegalArgumentException is thrown, but that's not enough
+		    //for the test.  We also need to make sure that the reference s is still null!
+		    assertNull(s);
+		}
+
+		// test null id
 		s = null;
 		try {
 		    s = new Student("first", "last", null, "email@ncsu.edu", "hashedpassword");
@@ -72,6 +98,19 @@ public class StudentTest {
 		    assertNull(s);
 		}
 		
+		//test empty id
+		s = null; //Initialize a student reference to null
+		try {
+		    s = new Student("first", "lastname", "", "email@ncsu.edu", "hashedpassword");
+		    //Note that for testing purposes, the password doesn't need to be hashedpassword
+		    fail(); //If we reach this point a Student was constructed when it shouldn't have been!
+		} catch (IllegalArgumentException e) {
+		    //We should get here if the expected IllegalArgumentException is thrown, but that's not enough
+		    //for the test.  We also need to make sure that the reference s is still null!
+		    assertNull(s);
+		}
+
+		//test null email
 		s = null; 
 		try {
 		    s = new Student("first", "last", "id", null, "hashedpassword");
@@ -81,6 +120,33 @@ public class StudentTest {
 		    //for the test.  We also need to make sure that the reference s is still null!
 		    assertNull(s);
 		}
+		
+		//test empty email
+		s = null; //Initialize a student reference to null
+		try {
+		    s = new Student("first", "last", "id", "", "hashedpassword");
+		    //Note that for testing purposes, the password doesn't need to be hashedpassword
+		    fail(); //If we reach this point a Student was constructed when it shouldn't have been!
+		} catch (IllegalArgumentException e) {
+		    //We should get here if the expected IllegalArgumentException is thrown, but that's not enough
+		    //for the test.  We also need to make sure that the reference s is still null!
+		    assertNull(s);
+		}
+		
+		//test last period before '@' sign
+		s = null; //Initialize a student reference to null
+		try {
+		    s = new Student("first", "last", "id", "email.ncsu@edu", "hashedpassword");
+		    //Note that for testing purposes, the password doesn't need to be hashedpassword
+		    fail(); //If we reach this point a Student was constructed when it shouldn't have been!
+		} catch (IllegalArgumentException e) {
+		    //We should get here if the expected IllegalArgumentException is thrown, but that's not enough
+		    //for the test.  We also need to make sure that the reference s is still null!
+		    assertNull(s);
+		}
+
+		
+
 		
 		s = null; 
 		try {
