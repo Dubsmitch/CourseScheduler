ddd11744635427abdc2e85086272de2034509a01
wsmitche <wsmitche@ncsu.edu>
2018-09-18 16:17:51 -0700

    Second long constructor created

--- PackScheduler/test/edu/ncsu/csc216/pack_scheduler/user/StudentTest.java
+++ PackScheduler/test/edu/ncsu/csc216/pack_scheduler/user/StudentTest.java
@@ -18,7 +18,47 @@ public class StudentTest {
 
 	@Test
 	public void testStudentStringStringStringStringStringInt() {
-		fail("Not yet implemented");
+		
+		//test creating a valid Student
+		Student s = null; //Initialize a student reference to null
+		try {
+			s = new Student ("first", "last", "id", "email@ncsu.edu", "hashedpassword", 18);
+			assertEquals("first", s.getFirstName());
+			assertEquals("last", s.getLastName());
+			assertEquals("id", s.getId());
+			assertEquals("email@ncsu.edu", s.getEmail());
+			assertEquals("hashedpassword", s.getPassword());
+			assertEquals(18, s.getMaxCredits());
+			
+		} catch (IllegalArgumentException e){
+			fail();
+		}
+		
+		//boundary test for <3 credits
+		s = null;
+		try {
+		    s = new Student("first", "last", "id", "email@ncsu.edu", "hashedpassword", 2);
+		    //Note that for testing purposes, the password doesn't need to be hashedpassword
+		    fail(); //If we reach this point a Student was constructed when it shouldn't have been!
+		} catch (IllegalArgumentException e) {
+		    //We should get here if the expected IllegalArgumentException is thrown, but that's not enough
+		    //for the test.  We also need to make sure that the reference s is still null!
+		    assertNull(s);
+		}
+		
+		//boundary test for >18 credits
+		s = null;
+		try {
+		    s = new Student("first", "last", "id", "email@ncsu.edu", "hashedpassword", 19);
+		    //Note that for testing purposes, the password doesn't need to be hashedpassword
+		    fail(); //If we reach this point a Student was constructed when it shouldn't have been!
+		} catch (IllegalArgumentException e) {
+		    //We should get here if the expected IllegalArgumentException is thrown, but that's not enough
+		    //for the test.  We also need to make sure that the reference s is still null!
+		    assertNull(s);
+		}
+				
+		
 	}
 
 	@Test
