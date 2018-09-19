55642cb096fa3f4287db2e52ab161d8701661ca8
wsmitche <wsmitche@ncsu.edu>
2018-09-18 18:54:05 -0700

    created (pasted) in the two helpers/imported stuff

---
+++ PackScheduler/test/edu/ncsu/csc216/pack_scheduler/io/StudentRecordIOTest.java
@@ -0,0 +1,86 @@
+package edu.ncsu.csc216.pack_scheduler.io;
+
+import static org.junit.Assert.*;
+
+import java.io.FileInputStream;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Scanner;
+
+import org.junit.Before;
+import org.junit.Test;
+/**
+ * This tests the StudentRecordIO.java class and interacts
+ * with testing files
+ * 
+ * @author William
+ *
+ */
+public class StudentRecordIOTest {
+
+	private String validStudent0 = "Zahir,King,zking,orci.Donec@ametmassaQuisque.com,pw,15";
+	private String validStudent1 = "Cassandra,Schwartz,cschwartz,semper@imperdietornare.co.uk,pw,4";
+	private String validStudent2 = "Shannon,Hansen,shansen,convallis.est.vitae@arcu.ca,pw,14";
+	private String validStudent3 = "Demetrius,Austin,daustin,Curabitur.egestas.nunc@placeratorcilacus.co.uk,pw,18";
+	private String validStudent4 = "Raymond,Brennan,rbrennan,litora.torquent@pellentesquemassalobortis.ca,pw,12";
+	private String validStudent5 = "Emerald,Frost,efrost,adipiscing@acipsumPhasellus.edu,pw,3";
+	private String validStudent6 = "Lane,Berg,lberg,sociis@non.org,pw,14";
+	private String validStudent7 = "Griffith,Stone,gstone,porta@magnamalesuadavel.net,pw,17";
+	private String validStudent8 = "Althea,Hicks,ahicks,Phasellus.dapibus@luctusfelis.com,pw,11";
+	private String validStudent9 = "Dylan,Nolan,dnolan,placerat.Cras.dictum@dictum.net,pw,5";
+
+	private String [] validStudents = {validStudent0, validStudent1, validStudent2, validStudent3, validStudent4, validStudent5,
+	        validStudent6, validStudent7, validStudent8, validStudent9};
+
+	private String hashPW;
+	private static final String HASH_ALGORITHM = "SHA-256";
+
+	@Before
+	public void setUp() {
+	    try {
+	        String password = "pw";
+	        MessageDigest digest = MessageDigest.getInstance(HASH_ALGORITHM);
+	        digest.update(password.getBytes());
+	        hashPW = new String(digest.digest());
+	        
+	        for (int i = 0; i < validStudents.length; i++) {
+	            validStudents[i] = validStudents[i].replace(",pw,", "," + hashPW + ",");
+	        }
+	    } catch (NoSuchAlgorithmException e) {
+	        fail("Unable to create hash during setup");
+	    }
+	}
+	private void checkFiles(String expFile, String actFile) {
+	    try {
+	        Scanner expScanner = new Scanner(new FileInputStream(expFile));
+	        Scanner actScanner = new Scanner(new FileInputStream(actFile));
+	        
+	        while (expScanner.hasNextLine()  && actScanner.hasNextLine()) {
+	            String exp = expScanner.nextLine();
+	            String act = actScanner.nextLine();
+	            assertEquals("Expected: " + exp + " Actual: " + act, exp, act);
+	        }
+	        if (expScanner.hasNextLine()) {
+	            fail("The expected results expect another line " + expScanner.nextLine());
+	        }
+	        if (actScanner.hasNextLine()) {
+	            fail("The actual results has an extra, unexpected line: " + actScanner.nextLine());
+	        }
+	        
+	        expScanner.close();
+	        actScanner.close();
+	    } catch (IOException e) {
+	        fail("Error reading files.");
+	    }
+	}
+	@Test
+	public void testReadStudentRecords() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testWriteStudentRecords() {
+		fail("Not yet implemented");
+	}
+
+}
