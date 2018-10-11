540affce7bd5a372dc8a181a5991da6a33246ea8
wsmitche <wsmitche@ncsu.edu>
2018-10-11 16:41:44 -0700

    ugh, git problems

--- PackScheduler/test/edu/ncsu/csc216/pack_scheduler/io/CourseRecordIOTest.java
+++ PackScheduler/test/edu/ncsu/csc216/pack_scheduler/io/CourseRecordIOTest.java
@@ -45,7 +45,7 @@ public class CourseRecordIOTest {
 	/**
 	 * Resets course_records.txt for use in other tests.
 	 * 
-	 * @throws Exception if files cannot be found/read
+	 * @throws Exception if files cannot be found/read.
 	 */
 	@Before
 	public void setUp() throws Exception {
