2d3018123509b11652a7786152700e1a285e4c5c
wsmitche <wsmitche@ncsu.edu>
2018-11-13 14:14:41 -0800

    compile!!!

--- PackScheduler/src/edu/ncsu/csc216/pack_scheduler/user/schedule/Schedule.java
+++ PackScheduler/src/edu/ncsu/csc216/pack_scheduler/user/schedule/Schedule.java
@@ -123,17 +123,19 @@ public class Schedule {
 	 * 			the course to be added to the schedule
 	 * @return boolean
 	 * 			whether or not the course can be added to the schedule
-	 * @throws ConflictException 
-	 * 		if there is conflict
 	 */
-	public boolean canAdd(Course course) throws ConflictException{
+	public boolean canAdd(Course course) {
 		if (course == null) {
 			return false;
 		}
 		
 		for (int i = 0; i < this.getSchedule().size(); i++) {
 			if (this.getSchedule().get(i).equals(course)) {
-				this.getSchedule().get(i).checkConflict(course);
+				try{
+					this.getSchedule().get(i).checkConflict(course);
+				} catch (ConflictException e) {
+					return false;
+				}
 				
 			}
 		}
